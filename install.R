b615da551abac24b90b41b83e7dd6cddff3ea0af
Nick McKay <nickmckay@users.noreply.github.com>
2022-04-13 15:50:41 -0700

    some text

--- install.R
+++ install.R
@@ -1,3 +1,6 @@
-install.packages("pacman",repos = "https://cran.rstudio.com/")
-pacman::p_load("dplyr","magrittr")
-pacman::p_load_gh("nickmckay/lipdR", "nickmckay/geoChronR", "NeotomaDB/neotoma2", "LinkedEarth/actR")
+install.packages("pacman")
+install.packages("remotes")
+
+remotes::install_github("nickmckay/geoChronR")
+remotes::install_github("NeotomaDB/neotoma2")
+remotes::install_github("LinkedEarth/actR")
