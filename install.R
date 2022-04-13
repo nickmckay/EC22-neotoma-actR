b2dbcbca775aa25fc2aee7694088056a941f31fd
Nick McKay <nickmckay@users.noreply.github.com>
2022-04-13 13:36:32 -0700

    nope to holepunch

---
+++ install.R
@@ -0,0 +1,3 @@
+install.packages("pacman",repos = "https://cran.rstudio.com/")
+pacman::p_load("dplyr","magrittr")
+pacman::p_load_gh("nickmckay/lipdR", "nickmckay/geoChronR", "NeotomaDB/neotoma2", "LinkedEarth/actR")
