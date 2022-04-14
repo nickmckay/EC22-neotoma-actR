8eb595ca4320db2676255367a8109c486191f3eb
Nick McKay <nickmckay@users.noreply.github.com>
2022-04-13 18:36:58 -0700

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,3 @@
 install.packages("pacman")
 install.packages("remotes")
-remotes::install_github("nickmckay/lipdR",ref = "neotoma2lipd")
-remotes::install_github("nickmckay/geoChronR")
 remotes::install_github("LinkedEarth/actR")
